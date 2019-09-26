fd821396ba3790deda7a92f08c25f831c7be2648
Matthew C <m23.cullen@gmail.com>
2019-09-26 13:57:17 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,14 @@
 class Waiter
-
+  attr_accessor :name, :age 
+  @@all = []
+  
+  def initialize(name,)
+    @name = name 
+    @age = age 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
 end
